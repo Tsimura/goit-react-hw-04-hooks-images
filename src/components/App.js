bcca357ecbfdc9186b18c2b69d43fdb5005f889d
Tsimura <chappa2828@gmail.com>
2022-01-16 22:38:35 +0200

    add ModalWindow and Toaster
    ...loading, Button, error!

--- src/components/App.js
+++ src/components/App.js
@@ -9,53 +9,88 @@ import ModalWindow from './Modal/Modal';
 import { AppWrapper } from './App.styled';
 
 export default function App() {
-  const [imageValue, setImageValue] = useState('');
-  // const [query, setQuery] = useState('');
-  // const [largeImage, setLargeImage] = useState('');
   const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
+  const [imageValue, setImageValue] = useState('');
+  const [currentPage, setСurrentPage] = useState(1);
+  // const [query, setQuery] = useState('');
+  const [largeImage, setLargeImage] = useState('');
   // const [loading, setLoading] = useState(false);
   // const [error, setError] = useState(null);
-  // const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     if (imageValue === '') {
       return;
     }
     imagesAPI
-      .fetchImages(imageValue)
-      .then(resp => {
-        console.log(resp.hits);
-        return resp.hits;
-      })
+      .fetchImages(imageValue, currentPage)
+      .then(response => response.hits)
       .then(respImg => {
-        console.log('respImg:', respImg);
-        // setCurrentPage();
-        return setImages(respImg);
+        console.log('Arr img_s:', respImg);
+        setImages(prevImg => [...prevImg, ...respImg]);
+        // setСurrentPage(prevCurrentPage => prevCurrentPage + 1);
       });
-  }, [imageValue]);
+  }, [currentPage, imageValue]);
 
-  // const setCurrentPage = () => {
-  //   setPage(prevState => prevState + 1);
-  // };
+  const handleFormSubmit = imageValue => {
+    setImageValue(imageValue);
+    setСurrentPage(1);
+    setImages([]);
+  };
 
-  const handleFormSubmit = imageValue => setImageValue(imageValue);
+  const handleGalleryItemClick = largeImageURL => {
+    setLargeImage(largeImageURL);
+    setShowModal(true);
+  };
+
+  const toggleModal = () => {
+    setShowModal(prev => !prev);
+    setLargeImage('');
+  };
+
   return (
-    <>
+    <AppWrapper>
       <Searchbar onSubmit={handleFormSubmit} />
 
-      <ImageGallery
-        images={images}
-        // onImageClick={this.handleGalleryItemClick}
+      <ImageGallery images={images} onImageClick={handleGalleryItemClick} />
+
+      {/* <Button onClick={setCurrentPage} /> */}
+
+      {/* {loading && <MyLoader style={{ marginRight: 'auto', marginLeft: 'auto' }} />;} */}
+
+      {showModal && (
+        <ModalWindow onClose={toggleModal}>
+          <img src={largeImage} alt="currentLargeImage" />
+        </ModalWindow>
+      )}
+
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 1000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+        }}
       />
-      {/* <Button loadMoreImages={getImageFetch} onClick={setCurrentPage} /> */}
-    </>
+    </AppWrapper>
   );
 }
 
 // ================================================
+// import React, { Component } from 'react';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import MyLoader from './Loader/Loader';
+// import imagesAPI from '../services/images-api';
+// import Searchbar from './Searchbar/Searchbar';
+// import ImageGallery from './ImageGallery/ImageGallery';
+// import Button from './Button/Button';
+// import ModalWindow from './Modal/Modal';
+// import { AppWrapper } from './App.styled';
 // class App extends Component {
-
 //   state = {
 //     imageValue: '',
 //     largeImage: '',
@@ -65,7 +100,6 @@ export default function App() {
 //     error: null,
 //     showModal: false,
 //   };
-
 //   componentDidUpdate(prevProps, prevState) {
 //     const prevRequestValue = prevState.imageValue;
 //     const nextRequestValue = this.state.imageValue;
@@ -78,25 +112,22 @@ export default function App() {
 //       this.scrollToBottom();
 //     }
 //   }
-
-// scrollToBottom = () => {
-//   window.scrollTo({
-//     top: document.documentElement.scrollHeight,
-//     behavior: 'smooth',
-//   });
-// };
-
-// handleFormSubmit = imageValue => {
-//   this.setState({ imageValue });
-// };
-
+//   scrollToBottom = () => {
+//     window.scrollTo({
+//       top: document.documentElement.scrollHeight,
+//       behavior: 'smooth',
+//     });
+//   };
+//   handleFormSubmit = imageValue => {
+//     this.setState({ imageValue });
+//   };
 //   getImageFetch = () => {
 //     const { imageValue } = this.state;
 //     imagesAPI
 //       .fetchImages(imageValue, this.state.currentPage)
 //       .then(images => {
 //         if (images.hits.length === 0) {
-//           toast.error('There is no result for your request!');
+//           toast.info('There is no result for your request!');
 //         }
 //         this.setState(prevState => ({
 //           images: [...prevState.images, ...images.hits],
@@ -105,41 +136,36 @@ export default function App() {
 //       .catch(error => this.setState({ error }))
 //       .finally(() => this.setState({ loading: false }));
 //   };
-
-// setCurrentPage = () => {
-//   this.setState(prevState => ({
-//     currentPage: prevState.currentPage + 1,
-//   }));
-// };
-
+//   setCurrentPage = () => {
+//     this.setState(prevState => ({
+//       currentPage: prevState.currentPage + 1,
+//     }));
+//   };
 //   handleGalleryItemClick = largeImageURL => {
 //     this.setState({ largeImage: largeImageURL, showModal: true });
 //   };
-
 //   toggleModal = () => {
 //     this.setState(prevState => ({
 //       showModal: !prevState.showModal,
 //       largeImage: '',
 //     }));
 //   };
-
 //   render() {
 //     const { loading, images, largeImage, error, showModal } = this.state;
-//     const showSearchResults = images.length;
-
+//     const showSearchResults = images.length; //!_1
 //     return (
 //       <AppWrapper>
 //         <Searchbar onSubmit={this.handleFormSubmit} />
 //         {showSearchResults > 0 && (
 //           <>
-// <ImageGallery
-//   images={images}
-//   onImageClick={this.handleGalleryItemClick}
-// />
-// <Button
-//   loadMoreImages={this.getImageFetch}
-//   onClick={this.setCurrentPage}
-// />
+//             <ImageGallery
+//               images={images}
+//               onImageClick={this.handleGalleryItemClick}
+//             />
+//             <Button
+//               loadMoreImages={this.getImageFetch}
+//               onClick={this.setCurrentPage}
+//             />
 //           </>
 //         )}
 //         {loading && (
@@ -151,17 +177,7 @@ export default function App() {
 //           </ModalWindow>
 //         )}
 //         {error && <h2>Sorry, something went wrong: {error.message}</h2>}
-//         <Toaster
-//           position="top-center"
-//           reverseOrder={false}
-//           toastOptions={{
-//             duration: 1000,
-//             style: {
-//               background: '#363636',
-//               color: '#fff',
-//             },
-//           }}
-//         />
+//         <ToastContainer position="top-center" autoClose={3000} />
 //       </AppWrapper>
 //     );
 //   }
